77bc6a55d64d16d260c57f5f737e9cd92dec1891
Suryanath G <121388374+surya08nath@users.noreply.github.com>
2025-01-19 15:18:28 +0530

    commented unwanted function

--- class-node/index.js
+++ class-node/index.js
@@ -24,15 +24,15 @@ program.command('count')
 program.parse(); 
 
 
-function main(filename){
-    fs.readFile(filename,"utf-8",function(err,data){
-        let total=0;
-        for (let i=0;i<data.length;i++){
-            if(data[i]===" "){
-                total++;
-            }
-        }
-        console.log(total+1);
-    })
-}
-main("a.txt");
+// function main(filename){
+//     fs.readFile(filename,"utf-8",function(err,data){
+//         let total=0;
+//         for (let i=0;i<data.length;i++){
+//             if(data[i]===" "){
+//                 total++;
+//             }
+//         }
+//         console.log(total+1);
+//     })
+// }
+// main("a.txt");
